8f362e612d740a654d9bf3f43e5feb452db12b36
vivek12345 <vivek.of.nayyar@gmail.com>
2015-02-23 04:42:16 +0530

    home page and booking design ready

--- js/bookShow.js
+++ js/bookShow.js
@@ -9,11 +9,11 @@ var settings=
 	height:35,
 	selectedSeatCss:'selectedSeat',
 	selectingSeatCss:'selectingSeat',
-	categories:
+	price:
 	{
-		bronze:5,
-		silver:8,
-		gold:10
+		bronze:80,
+		silver:100,
+		gold:150
 	}
 };
 
@@ -22,20 +22,47 @@ var selectingSeatArray=[];
 var createSeats=function(reservedSeat)
 {
 	var seatList=[],seatNumber,seatClass;
-	for(var i=0;i<settings.rows;i++)
+	var k=0;
+	for(var i=0;i<settings.rows;i++,k++)
 	{
 		for(var j=0;j<settings.column;j++)
 		{
+
 			seatNumber=(j+i*settings.column+1);
+			
+
+			if(seatNumber==51)
+				{
+
+					seatList.push("</ul></div>"+"<div class='silver_tickets_map'><p class='category'>SILVER (Rs "+settings.price.silver+")"+"</p>"+"<ul id='tickets_map'>");
+					k=0;
+				}
+				else if(seatNumber==151)
+				{
+					seatList.push("</ul></div>"+"<div class='bronze_tickets_map'><p class='category'>BRONZE (Rs "+settings.price.bronze+")"+"</p>"+"<ul id='tickets_map'>");
+					k=0;
+				}
+				else if(seatNumber==1)
+				seatList.push("<div class='gold_tickets_map'><p class='category'>GOLD (Rs "+settings.price.gold+")"+"</p>"+"<ul id='tickets_map'>");
+
 			seatClass=settings.seatCss+' '+settings.rowCssPrefix+i.toString()+' '+settings.colCssPrefix+j.toString();
+			
 			if ($.isArray(reservedSeat) && $.inArray(seatNumber, reservedSeat) != -1) 
 			{
                 seatClass += ' ' + settings.selectedSeatCss;
             }
-			seatList.push("<li class='"+seatClass+"'"+" style='top:"+(i*settings.height).toString()+"px;left:"+(j*settings.width).toString()+"px;'>"+"<a title='"+seatNumber+"'></a>"+"</li>");
+			//seatList.push("<li class='"+seatClass+"'"+" style='top:"+(i*settings.height).toString()+"px;left:"+(j*settings.width).toString()+"px;'>"+"<a title='"+seatNumber+"'></a>"+"</li>");
+			if(seatNumber%25==4||seatNumber%25==21)
+			{
+				seatClass += ' '+'invisible';
+				seatList.push("<li class='"+seatClass+"'"+" style='top:"+(k*settings.height+5).toString()+"px;left:"+(j*settings.width).toString()+"px;'>"+"</li>");
+			}
+			else
+				seatList.push("<li class='"+seatClass+"'"+" style='top:"+(k*settings.height+5).toString()+"px;left:"+(j*settings.width).toString()+"px;'>"+"<a title='"+seatNumber+"'></a>"+"</li>");
 		}
 	}
-	document.getElementById("tickets_map").innerHTML=seatList.join('');
+	//document.getElementById("tickets_map").innerHTML=seatList.join('');
+	document.getElementById("container").innerHTML=seatList.join('');
 };
 
 function sortNumber(a,b)
@@ -71,32 +98,58 @@ function isSingleSiloCreated(selectedSeatArray)
 		var start_seat=selectingSeatArray[0];
 		var end_seat=selectingSeatArray[selectingSeatArray.length-1];
 		var left=false,right=false;
-		if(start_seat%25!=1/*&&start_seat%25!=2*/)
-		left=checkLeft(start_seat);
-		if(end_seat%25!=0/*&&end_seat%25!=24*/)
-		right=checkRight(end_seat);
-		if(left&&right)
-		return true;
+		
+		if(start_seat%25!=1)
+		{
+			left=checkLeft(start_seat,end_seat);
+		}
+		
+		if(end_seat%25!=0)
+		{
+			right=checkRight(end_seat,start_seat);
+		}
+		if(left || right)
+		{
+			return true;
+		}
 	}
 	return false;
 }
 
-function checkLeft(start_seat)
+function checkLeft(start_seat,end_seat)
 {
-	if(!($.inArray(start_seat-1, bookedSeats) != -1))
+	if(start_seat%25!=2)
 	{
-		/*if(!($.inArray(start_seat-2, bookedSeats) != -1))*/
-		return true;
+		if(!($.inArray(parseInt(start_seat)-1, bookedSeats) != -1) && ($.inArray(parseInt(start_seat)-2,bookedSeats)!=-1) && !($.inArray(parseInt(end_seat)+1, bookedSeats) != -1))
+		{
+			return true;
+		}
+	}
+	else
+	{
+		if(!($.inArray(parseInt(start_seat)-1, bookedSeats) != -1))
+		{
+			return true;
+		}	
 	}
 	return false;
 }
 
-function checkRight(end_seat)
+function checkRight(end_seat,start_seat)
 {
-	if(!($.inArray(parseInt(end_seat)+1, bookedSeats) != -1))
+	if(end_seat%25!=24)
 	{
-		/*if(!($.inArray(end_seat+2, bookedSeats) != -1))*/
-		return true;
+		if(!($.inArray(parseInt(end_seat)+1, bookedSeats) != -1) && ($.inArray(parseInt(end_seat)+2,bookedSeats)!=-1) && !($.inArray(parseInt(start_seat)-1, bookedSeats) != -1)) 	
+		{
+			return true;
+		}
+	}
+	else
+	{
+		if(!($.inArray(parseInt(end_seat)+1, bookedSeats) != -1))
+		{
+			return true;
+		}
 	}
 	return false;
 }
@@ -131,10 +184,7 @@ window.onload=function()
 				else
 				{
 					$(this).toggleClass(settings.selectingSeatCss);
-					//seatArray.splice(($(this).children("a").attr('title'))-1,0,($(this).children("a").attr('title')));
 					selectingSeatArray.push($(this).children("a").attr('title'));
-					//console.log(selectingSeatArray);
-					//alert(no_of_seats);
 					if(--no_of_seats==0)
 					{
 						selectingSeatArray.sort(sortNumber);
